df01d73ac8f636e16bb21433f9919d85eb897523
Nicolas <nicolas.iparraguirre1710@gmail.com>
2022-02-17 00:04:53 -0500

    OpenBootcamp

---
+++ src/com/tarea/PrimeraTarea.java
@@ -0,0 +1,37 @@
+package com.tarea;
+
+public class PrimeraTarea {
+
+    public static void main(String[] args){
+
+        //1 Numericos
+        //1.1 Enteros
+        byte variable = 1;
+        System.out.println(variable);
+        short variable2 = 10;
+        System.out.println(variable2);
+        int variable3 = 83;
+        System.out.println(variable3);
+        long variable4 = 100;
+        System.out.println(variable4);
+        //1.2 Decimales
+        float variable5 = 5.5f;
+        System.out.println(variable5);
+        double variable6 = 21.8d;
+        System.out.println(variable6);
+
+        //2 Booleano
+        boolean variable7 = false;
+        System.out.println(variable7);
+        boolean variable8 = true;
+        System.out.println(variable8);
+
+        //3 texto
+        char variable9 = 'a';
+        System.out.println(variable9);
+        String variable10 = "Nicolas";
+        System.out.println(variable10);
+
+    }
+
+}
